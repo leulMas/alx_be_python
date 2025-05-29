e8a845267ac3ef3bf683039fbc5e163db09177b7
leul Masresha <leulmasresha19@gmail.com>
2025-05-29 20:10:18 +0300

    daily_reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,13 +1,13 @@
-task = input("Enter your task: ")
-priority = input("Priority (high/medium/low):").lower()
-time_bound = input("Is it time-bound? (yes/no) ").lower()
+Task = input("Enter your task: ")
+Priority = input("Priority (high/medium/low):").lower()
+Time_bound = input("Is it time-bound? (yes/no) ").lower()
 
 
-if priority == "high":
-    if time_bound == "yes":
-        print(f"Reminder: {task} is a high priority task that requires immediate attention today!")
-elif priority == "low":
-    if time_bound == "no":
-        print(f"Note: {task} is a low priority task. Consider completing it when you have free time.")
+if Priority == "high":
+    if Time_bound == "yes":
+        print(f"Reminder: {Task} is a high priority task that requires immediate attention today!")
+elif Priority == "low":
+    if Time_bound == "no":
+        print(f"Note: {Task} is a low priority task. Consider completing it when you have free time.")
 else:
     print("enter a valid task")
