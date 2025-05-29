eac5da19336c025528d801cd04fc1cc0a0cdb1c2
leul Masresha <leulmasresha19@gmail.com>
2025-05-29 19:04:54 +0300

    match_case_operator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,22 +1,22 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 opp = input("Choose the operation (+, -, *, /): ")
 
 match opp:
     case "+":
         result = num1 + num2
-        print("The result is" ,result)
+        print(f"The result is {result}")
     case "-":
         result = num1 - num2
-        print("The result is" ,result)
+        print(f"The result is {result}")
     case "*":
         result = num1 * num2
-        print("The result is" ,result)
+        print(f"The result is {result}")
     case "/":
         if num2 == 0:
             print("Cannot divide by zero.")
         else:
             result = num1 / num2
-            print("The result is" ,result)
+            print(f"The result is {result}")
     case _:
         print("you have entered a wrong operator")
