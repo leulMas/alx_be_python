26e5c6356b3c0b170d4f9d7d0077c75372a2c386
leul Masresha <leulmasresha19@gmail.com>
2025-05-29 17:43:02 +0300

    match_case_operator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -13,6 +13,6 @@ match opp:
         if num2 == 0:
             print("cannot divide by zero")
         else:
-            print(num1 / num2)
+            print(f"The result is {num1 / num2}")
     case _:
         print("you have entered a wrong operator")
