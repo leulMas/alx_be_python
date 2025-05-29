101c1c63124caddd2d6bbc0b76643cb88e2a82c6
leul Masresha <leulmasresha19@gmail.com>
2025-05-29 17:58:38 +0300

    match_case_operator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -5,18 +5,18 @@ opp = input("Choose the operation (+, -, *, /): ")
 match opp:
     case "+":
         result = num1 + num2
-        print("The result is", result)
+        print("The result is", [result])
     case "*":
         result = num1 * num2
-        print("The result is", result )
+        print("The result is", [result] )
     case "-":
         result = num1 - num2
-        print("The result is", result )
+        print("The result is", [result] )
     case "/":
         if num2 == 0:
             print("cannot divide by zero5")
         else:
             result = num1 / num2
-            print("The result is", result)
+            print("The result is", [result])
     case _:
         print("you have entered a wrong operator")
